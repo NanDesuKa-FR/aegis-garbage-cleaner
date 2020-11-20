8ad402e5c7ba5533cccdf82ae4e0112451c77062
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2020-11-20 14:29:51 +0100

    Add files via upload
    Modification of the script to apply a clean recursively

--- __main__.py
+++ __main__.py
@@ -1,15 +1,18 @@
-import glob
+from pathlib import Path
 
 if __name__ == "__main__":
-    for file in glob.glob("*.ass"):
-        f_read = open(file, "r")
-        content_file = f_read.read()
-        f_read.close()
-        if content_file.find("[Aegisub Project Garbage]") != -1:
-            aegis_garbage = (content_file.split("[Aegisub Project Garbage]")[1]).split("[V4+ Styles]")[0]
-            new_content = (content_file.replace(aegis_garbage, "")).replace("[Aegisub Project Garbage]", "")
-            file_update = open(file, 'w')
-            file_update.write(new_content)
-            file_update.close()
-        else:
-            print(file + " n'a pas de Aegis Garbage")
+    for p in  Path('.').glob('./**/*'):
+        name_file = str(p.name)[-4:]
+        if p.is_file() and name_file.find(".ass") != -1:
+            f_read = open(p, "r", encoding='utf8', errors='ignore')
+            content_file = f_read.read()
+            f_read.close()
+            if content_file.find("[Aegisub Project Garbage]") != -1:
+                print("[CLEAR] " + str(p.name))
+                aegis_garbage = (content_file.split("[Aegisub Project Garbage]")[1]).split("[V4+ Styles]")[0]
+                new_content = (content_file.replace(aegis_garbage, "")).replace("", "")
+                file_update = open(p, 'w', encoding='utf8', errors='ignore')
+                file_update.write(new_content)
+                file_update.close()
+            else:
+                print(str(p.name) + " n'a pas de Aegis Garbage")
